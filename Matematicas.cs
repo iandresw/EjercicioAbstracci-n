c4f28a69ec7e124b952f93692cfb041428613f2f
iandresw <rbejarano@unah.hn>
2024-08-02 20:34:36 -0600

    Create Matematicas.cs
    se creo la clase abstracta operaciones y se agrego 4  operaciones matemáticas y el método imprimir

---
+++ Matematicas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioAbstracción
+{
+    public  abstract class Matematicas
+    {
+        public abstract double Suma();
+        public abstract double Resta();
+        public abstract double Multiplicacion();
+        public abstract double Divicion();
+        public abstract void Imprimir();
+    }
+}
