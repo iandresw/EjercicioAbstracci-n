c20fddfd3ee58cc64a2a48376abfc33c9ff195f1
iandresw <rbejarano@unah.hn>
2024-08-02 20:36:14 -0600

    Update Program.cs
    se agrego y se mando a llamar la clase calculadora para implantar

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EjercicioAbstracción;
+
+Console.WriteLine("Ejercicio Abstraccion!");
+Calculadora calculadora = new Calculadora();
+Console.WriteLine("Ingrese el Primer Numero:");
+calculadora.Num1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el Segundo Numero:");
+calculadora.Num2 = double.Parse(Console.ReadLine());
+calculadora.Imprimir();
